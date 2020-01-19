37a5716f7fb081b7b78c0d2c1d0ab1265a76f599
Amol Patil <computer.warriors2020@gmail.com>
2020-01-20 03:19:03 +0530

    added sidebar

--- front-end-app/src/app/app.module.ts
+++ front-end-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header.component';
+import { SidebarComponent } from './shared/layout/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
     HeaderComponent
   ],
   imports: [
--- front-end-app/src/app/shared/layout/header.component.ts
+++ front-end-app/src/app/shared/layout/header.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 @Component({
     selector:'app-layout-header',
     templateUrl:'./header.component.html',
-    styleUrls:['./header.component.css']
 })
 
 export class HeaderComponent{
