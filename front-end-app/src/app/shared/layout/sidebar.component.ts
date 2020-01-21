ec1f2cc717ed08617251bc3e16041eb36b67f909
Amol Patil <apatil@mdsol.com>
2020-01-22 00:27:51 +0530

    added jquery and synamic side menu

--- front-end-app/src/app/shared/layout/sidebar.component.ts
+++ front-end-app/src/app/shared/layout/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var $: any;
 
 @Component({
     selector: 'app-layout-sidebar',
@@ -6,6 +8,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./sidebar.component.css']
 })
 
-export class SidebarComponent{
+export class SidebarComponent implements AfterViewInit{
+    ngAfterViewInit(): void {
+        $(".sidebar-dropdown > a").click(function () {
+            $(".sidebar-submenu").slideUp(200);
+            if (
+              $(this)
+                .parent()
+                .hasClass("active")
+            ) {
+              $(".sidebar-dropdown").removeClass("active");
+              $(this)
+                .parent()
+                .removeClass("active");
+            } else {
+              $(".sidebar-dropdown").removeClass("active");
+              $(this)
+                .next(".sidebar-submenu")
+                .slideDown(200);
+              $(this)
+                .parent()
+                .addClass("active");
+            }
+          });
+        
+          $("#close-sidebar").click(function () {
+            $(".page-wrapper").removeClass("toggled");
+          });
+          $("#show-sidebar").click(function () {
+            $(".page-wrapper").addClass("toggled");
+          });
+    }
 
 }
