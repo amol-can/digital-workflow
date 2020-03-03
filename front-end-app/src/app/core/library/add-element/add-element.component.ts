aa3202b617062f17271ebcf4cffbb38ce27a8711
Amol Patil <apatil@mdsol.com>
2020-03-03 18:50:14 +0530

    Readme for java app and add element section

--- front-end-app/src/app/core/library/add-element/add-element.component.ts
+++ front-end-app/src/app/core/library/add-element/add-element.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-add-element',
   templateUrl: './add-element.component.html',
   styleUrls: ['./add-element.component.css']
 })
-export class AddElementComponent implements OnInit {
+export class AddElementComponent implements OnInit,AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  ngAfterViewInit(): void{
+
+  }
+
+  
 
 }
