ebb61f5ab839d0f921f37ebef23419ee6be7ab65
Amol Patil <computer.warriors2020@gmail.com>
2020-03-16 08:48:11 +0530

    element relationship Search push, UI layout addded

--- front-end-app/src/app/core/library/element-relationship/element-relationship.component.ts
+++ front-end-app/src/app/core/library/element-relationship/element-relationship.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LibraryService } from 'src/app/services/library.service';
+import { StudyDesignElement } from 'src/app/services/StudyDesignElement';
+import { ElementRelationship } from 'src/app/services/model/element-relation-model';
 
 @Component({
   selector: 'app-element-relationship',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./element-relationship.component.css']
 })
 export class ElementRelationshipComponent implements OnInit {
+  sdeLibrary$: StudyDesignElement[];
 
-  constructor() { }
+  elementRelationship = new ElementRelationship('',null,false,'',null,false);
+
+  constructor(private relationService: LibraryService) { }
 
   ngOnInit() {
+    this.getStudyDesignElementLibrary();
+  }
+
+  getStudyDesignElementLibrary(){
+    this.relationService.getStudyDesignElement().subscribe(
+      response => this.sdeLibrary$ = response,
+      error => console.log('error while calling sde library: ', error)
+    );
   }
 
 }
