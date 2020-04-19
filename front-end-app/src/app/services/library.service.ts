3e230eb157737d7fca08e237b0795b168906a8d4
Amol Patil <computer.warriors2020@gmail.com>
2020-04-20 02:56:56 +0530

    extra parameter and library service header

--- front-end-app/src/app/services/library.service.ts
+++ front-end-app/src/app/services/library.service.ts
@@ -10,7 +10,10 @@ import { ElementValueRelationship } from './model/sde-value-relation-model';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json'
+    'Content-Type': 'application/json,Origin',
+    'Access-Control-Allow-Headers': 'Cache-Control',
+    'Access-Control-Allow-Methods': 'GET,POST,PATCH,DELETE,PUT,OPTIONS',
+    'Access-Control-Allow-Origin':'*'
   })
 };
 
@@ -28,70 +31,70 @@ export class LibraryService {
 
   /* 1. Study Design Element APIs */
 
-  addStudyDesignElement(data: StudyDesignElement) : Observable<any> {
-    console.log('custome post request data: ',data)
+  addStudyDesignElement(data: StudyDesignElement): Observable<any> {
+    console.log('custome post request data: ', data)
     return this.http.post(this.libraryUrl, data, httpOptions);
   }
 
-  getStudyDesignElement() : Observable<any>{
-    return this.http.get<StudyDesignElement>(this.libraryUrl,httpOptions);
+  getStudyDesignElement(): Observable<any> {
+    return this.http.get<StudyDesignElement>(this.libraryUrl, httpOptions);
   }
 
-  findStudyDesignElement(id: any): Observable<any>{
-    return this.http.get<StudyDesignElement>(this.libraryUrl+"/"+id,httpOptions);
+  findStudyDesignElement(id: any): Observable<any> {
+    return this.http.get<StudyDesignElement>(this.libraryUrl + "/" + id, httpOptions);
   }
 
   /* 2. Study Design Element Value APIs */
-  getStudyDesignElementValue(): Observable<any>{
-    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl,httpOptions);
+  getStudyDesignElementValue(): Observable<any> {
+    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl, httpOptions);
   }
 
-  getStudyDesignElementValueByName(elementName: any):Observable<any>{
-    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl,{ 
-      headers: new HttpHeaders({'Content-Type':  'application/json'}),
-      params:{
+  getStudyDesignElementValueByName(elementName: any): Observable<any> {
+    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      params: {
         elementName: elementName
       },
     });
   }
 
-  addStudyDesignElementValue(data: StudyDesignElementValue): Observable<any>{
+  addStudyDesignElementValue(data: StudyDesignElementValue): Observable<any> {
     return this.http.post<StudyDesignElementValue>(this.libraryValueUrl, data, httpOptions);
   }
 
-  findStudyDesignElementValue(id: any): Observable<any>{
-    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl+"/"+id,httpOptions);
+  findStudyDesignElementValue(id: any): Observable<any> {
+    return this.http.get<StudyDesignElementValue>(this.libraryValueUrl + "/" + id, httpOptions);
   }
 
-  studyDesignElementValueDelete(id: any):Observable<any>{
-    return this.http.delete(this.libraryValueUrl+"/"+id,httpOptions);
+  studyDesignElementValueDelete(id: any): Observable<any> {
+    return this.http.delete(this.libraryValueUrl + "/" + id, httpOptions);
   }
 
   /* 3. Element relationship APIs */
-  addStudyDesignElementRelationship(element : ElementRelationship): Observable<any>{
-    return this.http.post<ElementRelationship>(this.elementRelationshipUrl,element,httpOptions);
+  addStudyDesignElementRelationship(element: ElementRelationship): Observable<any> {
+    return this.http.post<ElementRelationship>(this.elementRelationshipUrl, element, httpOptions);
   }
 
-  getStudyDesignElementRelationship(elementName: String):Observable<any>{
-    return this.http.get<ElementRelationship>(this.elementRelationshipUrl+"/"+elementName,httpOptions);
+  getStudyDesignElementRelationship(elementName: String): Observable<any> {
+    return this.http.get<ElementRelationship>(this.elementRelationshipUrl + "/" + elementName, httpOptions);
   }
 
-  getStudyDesignElementRelationshipByName(sdElementName: string): Observable<any>{
-    return this.http.get<ElementRelationship>(this.elementRelationshipUrl,{ 
-      headers: new HttpHeaders({'Content-Type':  'application/json'}),
-      params:{
+  getStudyDesignElementRelationshipByName(sdElementName: string): Observable<any> {
+    return this.http.get<ElementRelationship>(this.elementRelationshipUrl, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      params: {
         elementName: sdElementName
       },
     });
   }
 
   /* 4. Element Value Relationship */
-  addStudyElementValueRelationship(element : ElementValueRelationship): Observable<any>{
-    return this.http.post<ElementValueRelationship>(this.elementValueRelationshipUrl,element,httpOptions);
+  addStudyElementValueRelationship(element: ElementValueRelationship): Observable<any> {
+    return this.http.post<ElementValueRelationship>(this.elementValueRelationshipUrl, element, httpOptions);
   }
 
-  getStudyElementValueRelationship():Observable<any>{
-    return this.http.get<ElementValueRelationship>(this.elementValueRelationshipUrl,httpOptions);
+  getStudyElementValueRelationship(): Observable<any> {
+    return this.http.get<ElementValueRelationship>(this.elementValueRelationshipUrl, httpOptions);
   }
 
 }
